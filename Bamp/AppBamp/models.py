8460e911b13cbf1f92dc35b2903f652622895fee
pardddo1 <markosagote@opendeusto.es>
2023-11-09 10:39:18 +0100

    ModelosCorregidos

--- Bamp/AppBamp/models.py
+++ Bamp/AppBamp/models.py
@@ -1,41 +1,41 @@
 from django.db import models
 
 class Ciudad(models.Model):
-    nombre = models.Charfield(max_length=50)
+    nombre = models.CharField(max_length=50)
 
 class CategoriaRestaurante(models.Model):
-    idCategoriaRestaurante = models.Charfield(max_length = 5)
-    nombreCategoriaRestaurante = models.Charfield(max_length = 50)
+    idCategoriaRestaurante = models.CharField(max_length = 5)
+    nombreCategoriaRestaurante = models.CharField(max_length = 50)
 
 class Restaurante(models.Model):
-    idRestaurante = models.Charfield(max_length = 5)
-    nombreRestaurante = models.Charfield(max_length = 50)
+    idRestaurante = models.CharField(max_length = 5)
+    nombreRestaurante = models.CharField(max_length = 50)
     idCategoriaRestaurante = models.ForeignKey(CategoriaRestaurante, on_delete = models.CASCADE)
 
 class Carta(models.Model):
-    idCarta = models.Charfield(max_length = 5)
-    numeroArticulos = models.integerField(default = 0)
+    idCarta = models.CharField(max_length = 5)
+    numeroArticulos = models.IntegerField(default = 0)
     idRestaurante = models.ForeignKey(Restaurante, on_delete = models.CASCADE)
 
 class Producto(models.Model):
-    idProducto = models.Charfield(max_length = 5)
-    nombreProducto = models.Charfield(max_length = 50)
-    descripcion = models.Textfield(max_length = 600)
-    precio = models.integerField()
+    idProducto = models.CharField(max_length = 5)
+    nombreProducto = models.CharField(max_length = 50)
+    descripcion = models.TextField(max_length = 600)
+    precio = models.IntegerField()
     idCarta = models.ForeignKey(Carta, on_delete = models.CASCADE)
 
 class Usuario(models.Model):
-    idUsuario = models.Charfield(max_length = 5)
-    nombreUsuario = models.Charfield(max_length = 50)
-    apellidoUsuario = models.Charfield(max_length = 50)
+    idUsuario = models.CharField(max_length = 5)
+    nombreUsuario = models.CharField(max_length = 50)
+    apellidoUsuario = models.CharField(max_length = 50)
     fechaNacimiento = models.DateTimeField()
-    direccion = models.Textfield()
-    email = models.Textfield()
+    direccion = models.TextField()
+    email = models.TextField()
     nombreCiudad = models.ForeignKey(Ciudad, on_delete = models.CASCADE)
 
 class Pedido(models.Model):
-    idPedido = models.Charfield(max_length = 5)
-    importePedido = models.integerField()
+    idPedido = models.CharField(max_length = 5)
+    importePedido = models.IntegerField()
     idUsuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
 
 class CategoriaRestauranteCiudad(models.Model):
