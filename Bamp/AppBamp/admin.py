33e30766490de8c843775c8a3f8da182e9265efe
pardddo1 <markosagote@opendeusto.es>
2023-11-09 12:35:32 +0100

    administrador

--- Bamp/AppBamp/admin.py
+++ Bamp/AppBamp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Ciudad, CategoriaRestaurante, Restaurante, Carta, Producto, Pedido, Usuario, CategoriaRestauranteCiudad, PedidoProducto
 
-# Register your models here.
+admin.site.register(Ciudad)
+admin.site.register(CategoriaRestaurante)
+admin.site.register(Restaurante)
+admin.site.register(Carta)
+admin.site.register(Producto)
+admin.site.register(Pedido)
+admin.site.register(Usuario)
+admin.site.register(CategoriaRestauranteCiudad)
+admin.site.register(PedidoProducto)
