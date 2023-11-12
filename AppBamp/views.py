1e8ee92608a7a758437fe4da118fc8dc99610422
Derau21 <114677106+Derau21@users.noreply.github.com>
2023-11-12 13:11:41 +0100

    crear templates

--- AppBamp/views.py
+++ AppBamp/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("hola")
+
+def inicio(request):
+    return render(request, 'AppBamp/inicio.html', context)
