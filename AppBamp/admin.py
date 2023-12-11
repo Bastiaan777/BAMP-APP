287c906701313c1d9e48f3f497ca8d0798a603df
alexcalderon17 <114677265+alexcalderon17@users.noreply.github.com>
2023-12-11 11:48:39 +0100

    avance bd

--- AppBamp/admin.py
+++ AppBamp/admin.py
@@ -3,8 +3,14 @@ from .models import Ciudad, CategoriaRestaurante, Restaurante, Producto, Pedido,
 
 admin.site.register(Ciudad)
 admin.site.register(CategoriaRestaurante)
-admin.site.register(Restaurante)
 admin.site.register(Producto)
 admin.site.register(Pedido)
 admin.site.register(PerfilUsuario)
 admin.site.register(PedidoProducto)
+
+class RestauranteAdmin(admin.ModelAdmin):
+    list_display = ('nombreRestaurante', 'categoriaRestaurante')  # Campos que quieres mostrar
+    list_filter = ('categoriaRestaurante',)  # Aquí agregas el campo por el cual filtrar
+    ordering = ['categoriaRestaurante']
+    
+admin.site.register(Restaurante, RestauranteAdmin)
